56834772a0ae45e1d42396df81cd373da2c0c5b3
alexo <alexogrebchuk@gmail.com>
2019-10-14 03:07:56 +0300

    Extend functionality

--- src/main/java/me/indef/util/ProductLinkBuilder.java
+++ src/main/java/me/indef/util/ProductLinkBuilder.java
@@ -7,7 +7,7 @@ public class ProductLinkBuilder {
     public static String build(String name, String brand, String id) {
         name = name.toLowerCase().replaceAll("\\W+", "-");
         if (!name.endsWith("-")) name = name + "-";
-        brand = brand.toLowerCase().replaceAll("\\W+", "-");
-        return baseUrl + brand + "/" + name + id;
+        brand = brand.toLowerCase().replaceAll("\\W+", " ").trim().replaceAll("\\W+", "-");
+        return baseUrl + brand + "/" + name + id.toLowerCase();
     }
 }
