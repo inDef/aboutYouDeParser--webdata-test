b11c8c01c6da22193a5f13b5c167f08c5a27b1f2
alexo <alexogrebchuk@gmail.com>
2019-10-14 03:05:00 +0300

    Add invalid input url behavior

--- src/main/java/me/indef/util/ProductJsonNodeProvider.java
+++ src/main/java/me/indef/util/ProductJsonNodeProvider.java
@@ -8,20 +8,26 @@ import org.jsoup.nodes.Document;
 public class ProductJsonNodeProvider {
 
     public static JsonNode getFromUrl(String url) {
+
+        if (!url.matches("https://.+/p/.+-\\d+")) return null;
+
         try {
             Document categoryPage = Jsoup.connect(url)
                     .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36")
+                    .maxBodySize(0)
                     .referrer("http://www.google.com").get();
             Counter.increaseHTTP();
 
             String elementJSON = categoryPage.getElementsByAttributeValue("charSet", "UTF-8")
-                    .get(2).html()
+                    .get(2)
+                    .html()
                     .split(",\"categoriesByPathname\"")[0]
-                    .split(",\"products\":")[1].split(",\"categories\":\\{")[0];
+                    .split(",\"products\":")[1]
+                    .split(",\"categories\":\\{")[0];
 
             String productId = url.split("-")[url.split("-").length - 1];
-
-            return new ObjectMapper().readTree(elementJSON).path(productId);
+            JsonNode productJsonNode = new ObjectMapper().readTree(elementJSON).path(productId);
+            if (!productJsonNode.toString().isEmpty()) return productJsonNode;
 
         } catch (Exception e) {
             e.printStackTrace();
